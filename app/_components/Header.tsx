eedf09f4ee1711f4d345d0db7a9dbb0eea85a3cd
Hardy <hardsyldev@gmail.com>
2024-08-20 14:22:48 +0300

    add github logo

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,16 +1,18 @@
-import { Section } from "./Section"
+import { Button } from "@/components/ui/button";
+import { Section } from "./Section";
 
 export const Header = () => {
-    return (
-        <header>
-            <Section className="flex items-baseline">
-                <h1 className="text-4-xl font-bold text-primary">
-                    hardev.com
-                </h1>
-                <p className="text-lg text-muted">
-                    Web developper
-                </p>
-            </Section>
-        </header>
-    )
-}
+  return (
+    <header className="sticky top-0">
+      <Section className="flex items-baseline">
+        <h1 className="text-4-xl font-bold text-primary">hardev.com</h1>
+        <div className="flex-1">
+          <ul>
+            <Button className="size-6"/>
+          </ul>
+        </div>
+        <p className="text-lg text-muted">Web developper</p>
+      </Section>
+    </header>
+  );
+};
